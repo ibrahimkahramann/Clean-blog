a718298d6abd028c600c0baad8991f7099a29790
ibrahimkahramann <ibrahimk.bg06@gmail.com>
2023-07-06 14:52:43 +0300

    readme and appjs created

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-const fn = function () {
-  console.log('Function working');
-};
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  const blog = {
+    id: 1,
+    title: 'Blog title',
+    description: 'Blog description',
+  };
+  res.send(photo);
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda baslatildi..`);
+});
--- readme.md
+++ readme.md
@@ -1,1 +1,2 @@
 # Clean blog
+## patika.dev nodejs egitimde olusturulmus proje
