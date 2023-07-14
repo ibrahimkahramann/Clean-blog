6d7a6d55bec32f95ce9aabce571e937a5d393b51
ibrahimkahramann <ibrahimk.bg06@gmail.com>
2023-07-14 15:31:32 +0300

    Dynamic content added

--- app.js
+++ app.js
@@ -1,27 +1,44 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
 const path = require('path');
+const Posts = require('./models/Posts');
 
 const app = express();
+
+//Connect MONGODB
+
+mongoose.connect('mongodb://127.0.0.1:27017/cleanblog-test-db');
 
 //TEMPLATE ENGINE
 app.set('view engine', 'ejs');
 
 //MIDDLEWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //ROUTES
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const allPosts = await Posts.find({});
+  res.render('index', {
+    allPosts,
+  });
 });
+
 app.get('/about', (req, res) => {
   res.render('about');
+});
+app.get('/post', (req, res) => {
+  res.render('post');
 });
 app.get('/add_post', (req, res) => {
   res.render('add_post');
 });
-app.get('/post', (req, res) => {
-  res.render('post');
+
+app.post('/posts', async (req, res) => {
+  await Posts.create(req.body);
+  res.redirect('/');
 });
 
 const port = 3000;
